ebd6872a1e22cb7ab1bd6587f825c1c0e1260dbd
Mansi Mansukh <mansimansukh.ai@gmail.com>
2026-03-12 07:57:43 +0530

    Added first hello program

---
+++ dictionary_example.py
@@ -0,0 +1,8 @@
+student = {
+    "name": "Mansi",
+    "age": 21,
+    "course": "Computer Engineering"
+}
+
+print(student["name"])
+print(student["age"])
---
+++ lists_example.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.append(6)
+
+for num in numbers:
+    print(num)
+    
